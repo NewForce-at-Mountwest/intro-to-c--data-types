602853d30c56a2e035d36c4e82c789ee729dc5a5
Jordan Castelloe <jordan.castelloe@gmail.com>
2020-07-14 13:14:44 -0400

    example code from day one

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("What's your name?");
+            string firstName = Console.ReadLine();
+
+            int milesPerHour = 30;
+
+            Console.WriteLine($"Hello, {firstName}");
+
+            if(milesPerHour > 20){
+                Console.WriteLine("You are extremely fast");
+            } else {
+                Console.WriteLine("You're pretty fast, but not as fast as Pat");
+            }
+
+        }
+    }
+}
